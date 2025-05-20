6c3dadd7c5235d586d304ec9539b69ca2cfba79c
Andras Solyom <andras.solyom@genesys.com>
2025-05-20 17:10:45 +0200

    [Update] 1.14.0

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GenesysCloud",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "GenesysCloud",
+            targets: ["GenesysCloudWrapper"]),
+    ],
+    dependencies: [
+        .package(
+            name: "GenesysCloudMessenger",
+            url: "https://github.com/MyPureCloud/mm-genesyscloudmessenger-spm-dev.git",
+            .exact("1.14.0")
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GenesysCloud",
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_t1.14.0.rc15_commit_e98fd80d7abaeaf376b046e9fd25c873f389365e.zip",
+            checksum: "a1e51c70392f387992218ff3f732df779e482be5b0c7a59f0cd4c07fbce649cf"
+        ),
+        .target(
+            name: "GenesysCloudWrapper",
+            dependencies: [
+                "GenesysCloud",
+                .product(name: "GenesysCloudMessenger", package: "GenesysCloudMessenger")
+            ]
+        ),
+        .testTarget(name: "PackageTests", dependencies: [
+            "GenesysCloud"
+        ])
+    ]
+)
