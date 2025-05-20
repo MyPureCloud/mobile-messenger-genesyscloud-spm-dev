33ac8f580958facf5383f6238ed54b8550184eb7
Andras Solyom <andras.solyom@genesys.com>
2025-05-20 17:15:55 +0200

    update test

--- Tests/PackageTests/Tests.swift
+++ Tests/PackageTests/Tests.swift
@@ -3,6 +3,6 @@ import GenesysCloud
 
 final class PackageTests: XCTestCase {
     func testImport() {
-        XCTAssertNotNil(GenesysCloud.ChatUIViewController.self)
+        XCTAssertNotNil(GenesysCloud.ChatController.self)
     }
 }
