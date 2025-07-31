bc11d30d1ad2a5235110f22faa6e996885b6a4c8
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2025-07-31 07:58:28 +0000

    [Fix] 1.15.0

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
         .package(
             name: "GenesysCloudMessenger",
             url: "https://github.com/MyPureCloud/mm-genesyscloudmessenger-spm-dev.git",
-            .exact("1.16.0")
+            .exact("1.15.0")
         )
     ],
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_t1.16.0.rc4_commit_e0e82951a159580b8879b13cb12e642fbb336dd3.zip",
-            checksum: "12431547df72b5eac2c9d59df3c10b95416922d58d5fd84a49759e51f92023d6"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_t1.15.0.rc53_commit_363d23197762d44e9c15089d16decdf1250efc7a.zip",
+            checksum: "f1a633c3767e73c3a0b62e6c8f364ae3d041f16fd773cf23f066efbcfbace921"
         ),
         .target(
             name: "GenesysCloudWrapper",
