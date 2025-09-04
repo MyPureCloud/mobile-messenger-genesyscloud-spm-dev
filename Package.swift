fdfa0b2e974d69007613863cd551e0ed48b94ddb
ec2-user <ec2-user@ip-10-25-189-85.ec2.internal>
2025-09-04 09:53:55 +0000

    [Fix] 1.16.1

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
         .package(
             name: "GenesysCloudMessenger",
             url: "https://github.com/MyPureCloud/mm-genesyscloudmessenger-spm-dev.git",
-            .exact("1.17.0")
+            .exact("1.16.1")
         )
     ],
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.17.0.rc1_commit_0fa0b6b1038fef5a9de9051e2cfcde0bc55e3b18.zip",
-            checksum: "4f1b5db1a137bb26fba9ae06c28a844c2a4b390cdd356db9efebdc6671baf1a8"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.16.1-SNAPSHOT-PR-16-4_commit_8b2477165cff2a7fb22b56d957399af1c83575c9.zip",
+            checksum: "529c7e7c95c66289081f9851a5d0a030f3c6e84ac6a68696cf070fdd46e54386"
         ),
         .target(
             name: "GenesysCloudWrapper",
