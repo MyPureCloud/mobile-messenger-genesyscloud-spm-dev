32aaa4a59dd47dd13f049de8a713d93dddb046a4
ec2-user <ec2-user@ip-10-25-189-85.ec2.internal>
2025-09-30 12:25:52 +0000

    [Fix] 0.0.2

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_0.0.2-SNAPSHOT-PR-52-14_commit_506e216e77476a344c111e9e3d9c1161d70f9312.zip",
-            checksum: "07ad7810519ba4a1f988596cddc1dbcfd3277d8bcb4db08a3c4f402e9a13a4d0"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_0.0.2-SNAPSHOT-PR-52-16_commit_e1a0cf580ece342e5f51822b84fd6679677e1854.zip",
+            checksum: "55948c4badd58047478e62551681ff0b09cbc0924921953c464cb44d7043703c"
         ),
         .target(
             name: "GenesysCloudWrapper",
