6ae74e0813a243e8b6b3c9efb64011fff668a650
ec2-user <ec2-user@ip-10-25-189-85.ec2.internal>
2025-10-21 17:20:49 +0000

    [Fix] 1.20.0

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.20.0-SNAPSHOT-feature-GMMS-9716-markdown-21_commit_a541ef7167c1c89e33cfe2ccdf457fc3295494e4.zip",
-            checksum: "614a0e48f4d41cd9a8c9dd690a3446f5bf755c8280a64ca6fd3ff09b9c6d4ea2"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.20.0-SNAPSHOT-feature-GMMS-9716-markdown-22_commit_a541ef7167c1c89e33cfe2ccdf457fc3295494e4.zip",
+            checksum: "a664551490a904a5fbfe353ea49d6806da9f02cfbbda5b717d345d03544d16a8"
         ),
         .target(
             name: "GenesysCloudWrapper",
