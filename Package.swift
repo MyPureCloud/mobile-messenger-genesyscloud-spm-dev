4a54314a10eedfc995d4d852786d4cd17fbe0b6c
ec2-user <ec2-user@ip-10-25-189-85.ec2.internal>
2025-10-29 08:45:21 +0000

    [Fix] 1.19.0

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.19.0-SNAPSHOT-PR-71-7_commit_4cb1f4ed558a7963d48c57437459c0c0492a6366.zip",
-            checksum: "1ec4c3e53762aa99d1cfa673f25c8a244454902eb6431a02895209a5151c81c3"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloud/GenesysCloud_version_1.19.0-SNAPSHOT-PR-71-10_commit_949a87c270b4ee6504a68c79de26cde74f41d819.zip",
+            checksum: "69c90a2cb4c6e3790d526f3c8b87215d977cae1a1c5691fb10b75c93925b0149"
         ),
         .target(
             name: "GenesysCloudWrapper",
